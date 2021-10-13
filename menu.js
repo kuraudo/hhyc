0c5f6c848dffd9451f4891a66649bb23373e3516
kuraudo <bgc.krd88@gmail.com>
2021-10-13 13:09:49 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-module.exports.menu = function menu() {
+window.menu = function menu() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
