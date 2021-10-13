ecddd4f55d5b9406b5b5b72d9b34dcea905d3737
kuraudo <bgc.krd88@gmail.com>
2021-10-13 12:47:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+export function menu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
