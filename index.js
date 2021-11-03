b6fb06438eb9aaf2f6f6cc899aa951f5c638e7bc
kuraudo <bgc.krd88@gmail.com>
2021-11-03 11:56:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ import 'slick-carousel'
     
 window.jQuery = window.$ = $; 
 
-$('.carousel-selector').slick({
-    //options
+$(".carousel").slick({
+ 
+  // normal options...
+  infinite: false,
+ 
+  // the magic
+  responsive: [{
+ 
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+ 
+    }, {
+ 
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+ 
+    }, {
+ 
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+ 
+    }]
 });
