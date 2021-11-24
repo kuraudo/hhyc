4acd746f4e42f7761016547f1fff6ae8d251c44a
Bernardo <bgc.krd88@gmail.com>
2021-11-24 17:38:45 +0100

    Paso de parcel v1 a v2

---
+++ dist/enlaces.1e70b246.js
@@ -0,0 +1,2 @@
+window.menu=function(){var e=document.getElementById("myLinks");"block"===e.style.display?e.style.display="none":e.style.display="block"};
+//# sourceMappingURL=enlaces.1e70b246.js.map
---
+++ dist/enlaces.81731e95.js
@@ -0,0 +1,2 @@
+window.menu=function(){var e=document.getElementById("myLinks");"block"===e.style.display?e.style.display="none":e.style.display="block"};
+//# sourceMappingURL=enlaces.81731e95.js.map
