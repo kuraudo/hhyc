b295d1f9b72da8b5aefd035e31abd41bf198ad76
kuraudo <bgc.krd88@gmail.com>
2021-12-27 10:10:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,46 @@ import $ from 'jquery'
 import 'jquery'
 import 'slick-carousel'
 import '@fortawesome/fontawesome-free/css/all.css'
-import 'jquery-lazy/jquery.lazy.min.js'
+import 'jquery-lazy'
+
+window.jQuery = window.$ = $; 
+
+$(".carousel").slick({
+ 
+ 
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    cssEase: 'linear',
+    infinite: true,
+    arrows: false,
+    responsive: [{
+ 
+      breakpoint: 1024,
+      settings: {
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+      }
+ 
+    }, {
+ 
+      breakpoint: 600,
+      settings: {
+        dots: true,
+        autoplay: false,
+      }
+ 
+    }, {
+ 
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+ 
+    }]
+});
+$(function() {
+        $('.lazy').lazy();
+});
